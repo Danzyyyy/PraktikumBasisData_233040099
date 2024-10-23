11dd480bee0abc58e00ba9b6e47d36b6ebd35023
Danzyyyy <tubagus.233040099@mail.unpas.ac.id>
2024-10-23 11:08:43 +0700

    add Tugas 3

---
+++ Pertemuan5/Tugas3_233040099/Latihan5.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO Dosen_Wali (NIP, nama, tanggal_lahir, alamat)
+VALUES ('980016587345616183', 'Zulfa Namaga', '1973-03-22', 'Gg. Antapani Lama No. 3, Padangpanjang, Banten 11586'),
+       ('052701456614374353', 'Gada Salahudin', '1992-04-10', 'Gang Ir. H. Djuanda No. 857, Bogor, MA 61948'),
+       ('604385402067652643', 'Mustika Anggriawan, S.Sos', '2002-04-09', 'Gg. Siliwangi No. 1, Serang, LA 69103'),	
+	   ('865769870963014234', 'Tgk. Ulva Ardianto', '1980-10-13', 'Gg. Rajawali Timur No. 3, Padang, Aceh 15705'),	
+	   ('816027390854818777', 'Ida Maulana', '1965-05-24', 'Gg. Jakarta No. 263, Kota Administrasi Jakarta Selatan, GO 93467'),
+	   ('731332769692891286', 'Hendri Pradana', '1984-05-21', 'Gang Jend. A. Yani No. 723, Gorontalo, BB 55706'),
+	   ('724474281012871858', 'Chandra Yolanda, S.H.', '1996-06-01', 'Jalan Raya Ujungberung No. 727, Balikpapan, Gorontalo 39369'),
+	   ('268408287117510784', 'Anita Anggraini', '1989-10-08', 'Jl. Raya Ujungberung No. 801, Subulussalam, SG 86953'),
+	   ('955643122246859672', 'Zelda Saefullah, S.Gz', '1983-11-15', 'Jalan Lembong No. 393, Malang, Bali 63316'),
+	   ('302051088374918223', 'Hari Suwarno, S.IP', '1995-05-30', 'Gg. Gedebage Selatan No. 7, Banjar, Banten 03803');
+
